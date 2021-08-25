3dfd0a458624bd625ec8ad9430ed6404d633462c
Mike Orozco <me@mikeorozco.dev>
2021-08-25 12:11:52 -0700

    Separate meta generation from esbuild config

--- esbuild.config.cjs
+++ esbuild.config.cjs
@@ -2,29 +2,9 @@ const { build } = require('esbuild');
 const packageJson = require('./package.json');
 const inlineImportPlugin = require('esbuild-plugin-inline-import');
 const { sassPlugin } = require('esbuild-sass-plugin');
+const meta = require('./meta.cjs');
 
-const { main, name, version, description, author, betterdiscord, homepage } =
-  packageJson;
-
-const metaComment = {
-  name,
-  version: version ?? '0.0.1',
-  description,
-  author: author?.name ?? author ?? undefined,
-  authorLink: author.url ?? homepage ?? undefined,
-  authorId: betterdiscord?.discord_snowflake,
-  updateUrl: betterdiscord?.update_url,
-  website: betterdiscord?.website,
-  source: betterdiscord?.source,
-  invite: betterdiscord?.invite,
-  donate: betterdiscord?.donate,
-  patreon: betterdiscord?.patreon,
-};
-Object.keys(metaComment).forEach((key) => {
-  if (metaComment[key] === undefined) {
-    delete metaComment[key];
-  }
-});
+const { main, name } = packageJson;
 
 const bdApiIgnore = {
   name: 'bdapi-ignore',
@@ -52,11 +32,7 @@ const options = {
   ],
   external: ['@bandagedbd/bdapi'],
   banner: {
-    js:
-      Object.entries(metaComment).reduce(
-        (acc, [key, value]) => `${acc}\n * @${key} ${value}`,
-        '/**'
-      ) + '\n */\n',
+    js: meta,
   },
 };
 
